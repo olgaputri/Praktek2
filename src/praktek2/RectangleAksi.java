a68ff628079189e8e10465f2c7d5c263a5d5c7f5
olgaputri <olgaputri654321@gmail.com>
2018-04-03 11:08:59 +0800

    Constructor dengan Parameter

--- src/praktek2/RectangleAksi.java
+++ src/praktek2/RectangleAksi.java
@@ -13,5 +13,8 @@ public class RectangleAksi {
             
         Rectangle r2 = new Rectangle();
             r2.cetakinfo();
+            
+        Rectangle r3 = new Rectangle(50,30);
+            r3.cetakinfo();
     }
 }
