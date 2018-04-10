e8c6a466809a601581905437c64486f7758e3fff
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-04-10 11:19:50 +0800

    Perbaikan Praktek2 Rectangle diganti Balok

--- src/praktek2/Balok.java
+++ src/praktek2/Balok.java
@@ -1,29 +1,33 @@
 package praktek2;
 
-public class Rectangle {
+public class Balok {
     double panjang;
     double lebar;
+    double tinggi;
 
-    public Rectangle() {
+    public Balok() {
         panjang=5;
         lebar=3;
+        tinggi=7;
     }
 
-    public Rectangle(double panjang, double lebar) {
+    public Balok(double panjang, double lebar, double tinggi) {
         this.panjang = panjang;
         this.lebar = lebar;
+        this.tinggi = tinggi;
     }
     
     void cetakinfo(){
         System.out.println("====================");
         System.out.println("Panjang : "+panjang);
         System.out.println("lebar   : "+lebar);
+        System.out.println("Tinggi   : "+tinggi);
         System.out.println("====================");
     }
     
     double hitungluas(){
         double luas;
-        luas = panjang*lebar;
+        luas = 2*(panjang*lebar+panjang*tinggi+lebar*tinggi);
         return luas;
     }
     
--- src/praktek2/BalokAksi.java
+++ src/praktek2/BalokAksi.java
@@ -1,20 +1,21 @@
 package praktek2;
 
-public class RectangleAksi {
+public class BalokAksi {
     public static void main(String[] args) {
         
-        Rectangle r1 = new Rectangle();
+        Balok r1 = new Balok();
             r1.panjang = 9;
             r1.lebar = 3;
+            r1.tinggi = 12;
             
             r1.cetakinfo();
             System.out.println("Luasnya adalah : "+r1.hitungluas());
             r1.cetakluas();
             
-        Rectangle r2 = new Rectangle();
+        Balok r2 = new Balok();
             r2.cetakinfo();
             
-        Rectangle r3 = new Rectangle(50,30);
+        Balok r3 = new Balok(21,16,26);
             r3.cetakinfo();
     }
 }
